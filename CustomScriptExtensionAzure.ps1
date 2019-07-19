cb673a50402fdaf56d96d26bb488bd4b904c4489
OanaCostina <52962680+OanaCostina@users.noreply.github.com>
2019-07-19 15:55:12 +0300

    Update CustomScriptExtensionAzure.ps1

--- CustomScriptExtensionAzure.ps1
+++ CustomScriptExtensionAzure.ps1
@@ -1,10 +1,10 @@
 #Change CD drive letter
 $driveletter = $args[0]
-$drv = Get-WmiObject win32_volume -filter "DriveLetter = `"$driveletter`""
+$drv = Get-WmiObject win32_volume -filter "DriveLetter = `"$($driveletter):`""
 if ($drv) {
 $drv.DriveLetter = "L:"
 $drv.Put() | out-null
 }
 
 $disk = Get-Disk | where partitionstyle -EQ 'raw' 
-$disk | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -UseMaximumSize -DriveLetter $driveletter[0] | Format-Volume -FileSystem NTFS -NewFileSystemLabel "UserData" -Confirm:$false -Force
+$disk | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -UseMaximumSize -DriveLetter $driveletter | Format-Volume -FileSystem NTFS -NewFileSystemLabel "UserData" -Confirm:$false -Force
