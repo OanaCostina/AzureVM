760d7b151ef5bef2bbf9ae5d80be222816b7da3e
OanaCostina <52962680+OanaCostina@users.noreply.github.com>
2019-08-06 12:25:21 +0300

    Update CustomScriptExtensionAzure.ps1

--- CustomScriptExtensionAzure.ps1
+++ CustomScriptExtensionAzure.ps1
@@ -1,15 +1,17 @@
 #Change CD drive letter
 [CmdletBinding()]
 param (
-    $drvLetter
+    $map
 )
 
-$driveletter = $drvLetter
-$drv = Get-WmiObject win32_volume -filter "DriveLetter = `"$($driveletter):`""
-if ($drv) {
-$drv.DriveLetter = "Z:"
-$drv.Put() | out-null
+$drives = $map | ConvertFrom-Json
+foreach ($drive in $drives) {
+    $driveletter = $drive.letter
+    $drv = Get-WmiObject win32_volume -filter "DriveLetter = `"$($driveletter):`""
+    if ($drv -and ($drv.DriveLetter -eq 'E:')) {
+        $drv.DriveLetter = "Z:"
+        $drv.Put() | out-null
+    }
+    $disk = Get-Disk | Select-Object *,@{Name='DiskSize'; Expression = {$_.size/1GB}} | where {$_.partitionstyle -EQ 'raw' -and $_.disksize -eq $drive.size}
+    $disk[0] | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -UseMaximumSize -DriveLetter $driveletter | Format-Volume -FileSystem NTFS -NewFileSystemLabel "UserData" -Confirm:$false -Force
 }
-
-$disk = Get-Disk | where partitionstyle -EQ 'raw' 
-$disk | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -UseMaximumSize -DriveLetter $driveletter | Format-Volume -FileSystem NTFS -NewFileSystemLabel "UserData" -Confirm:$false -Force
